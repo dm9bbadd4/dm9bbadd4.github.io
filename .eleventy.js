d8900b74bcc4eaacdc94f825d3aac81536074901
ollieswag <oliver.sutton@student.griffith.ie>
2025-05-29 18:21:54 +0100

    Added 11ty image utility

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addPassthroughCopy({
@@ -5,6 +6,9 @@ module.exports = function (eleventyConfig) {
         "node_modules/@glidejs/glide/dist/css/glide.theme.min.css": "assets/css/glide.theme.min.css",
         "node_modules/@glidejs/glide/dist/glide.min.js": "assets/js/glide.min.js"
     });
+
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
 
     return {
         dir: {
